1eccd213fdbe293a86e0f1c0b3c2f06bf4eb7367
aswe323 <matanwx135@gmail.com>
2025-01-16 13:53:23 +0200

    updated constants, player, main. added fire delay

--- player.py
+++ player.py
@@ -7,10 +7,11 @@ class Player(CircleShape):
     def __init__(self, x,y):
         super().__init__(x,y,constants.PLAYER_RADIUS)
         self.rotation = 0
+        self.fire_delay = 0
 
     def shoot(self):
         new_shot = Shot(x = self.position.x, y = self.position.y)
-        new_shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) #* constants.PLAYER_SHOT_SPEED
+        new_shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * constants.PLAYER_SHOT_SPEED
         #this causes a funny bug so I kept it :D
         # this applies the shot velocity to the player making it go really fast
         # I name it the "ramming speed" bug 
@@ -31,6 +32,8 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += constants.PLAYER_ROTATION_SPEED * dt
     def update(self, dt):
+        if (self.fire_delay > 0):
+            self.fire_delay -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -47,7 +50,11 @@ class Player(CircleShape):
             # ?
         if keys[pygame.K_SPACE]:
             #### this also updates the speed and velocity of the player for some reason Q_Q
-            self.shoot()
+            if (self.fire_delay > 0):
+                pass
+            else:
+                self.fire_delay += constants.PLAYER_SHOT_DELAY
+                self.shoot()
             # ?
 
     def move(self, dt):
